c52dd0640028bd7a51579386ca3c3b0e36a6c875
ryota-09 <n.ryo950911@gmail.com>
2022-08-11 15:58:02 +0900

    styled-componentsの初期設定

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  compiler: {
+    //NOTE: styledComponentsの有効化(SSRでも動作するため)
+    styledComponents: true
+  }
 }
 
 module.exports = nextConfig
