04cd1106615f824540420b7ee145d3748f24534f
ryota-09 <n.ryo950911@gmail.com>
2022-08-11 17:54:35 +0900

    storybookの設定

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,7 @@
+
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const path = require("path");
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -7,9 +11,24 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-postcss"
   ],
   "framework": "@storybook/react",
+  "staticDirs": ['public'],
+  "plugins": [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-private-methods',
+    '@babel/plugin-proposal-private-property-in-object',
+  ],
+  "webpackFinal": async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json')
+      }),
+    ]
+    return config
+  },
   "core": {
     "builder": "@storybook/builder-webpack5"
   }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import { ThemeProvider } from "styled-components"
+import { createGlobalStyle } from "styled-components"
+
+import { theme } from "../src/themes"
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +12,46 @@ export const parameters = {
     },
   },
 }
+
+export const GlobalStyle = createGlobalStyle`
+html,
+body,
+   textarea {
+     padding: 0;
+     margin: 0;
+     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+   }
+   * {
+     box-sizing: border-box;
+   }
+   a {
+     text-decoration: none;
+     transition: .25s;
+     color: #000000;
+   }
+`
+// Themeの適用
+addDecorator((story) => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    {story()}
+  </ThemeProvider>
+))
+
+// next/imageはstorybook上で動作しないため、差し替えている
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => typeof props.src === 'string' ? (
+    <OriginalNextImage {...props} unoptimized blurDataURL={props.src} />
+  ) : (
+    <OriginalNextImage {...props} unoptimized />
+  ),
+})
+
+Object.defineProperty(NextImage, '__esModule', {
+  configurable: true,
+  value: true,
+})
